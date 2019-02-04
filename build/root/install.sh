f35eedb92869bf7f5f4d338880e20171af1ad05f
binhex <megalith01@gmail.com>
2019-02-04 14:53:18 +0000

    remove mono from build

--- build/root/install.sh
+++ build/root/install.sh
@@ -19,7 +19,7 @@ mv /tmp/scripts-master/shell/arch/docker/*.sh /root/
 ####
 
 # define pacman packages
-pacman_packages="git libmediainfo mono sqlite chromaprint"
+pacman_packages="git libmediainfo sqlite chromaprint"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
